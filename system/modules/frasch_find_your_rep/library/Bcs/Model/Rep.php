759006bef0e306f53afef2021e1d1054d1b3e083
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-29 14:57:13 -0400

    Create Rep.php

---
+++ system/modules/frasch_find_your_rep/library/Bcs/Model/Rep.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find Your Rep
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-your-rep
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+namespace Bcs\Model;
+
+class Rep extends \Model
+{
+	/* Table name */
+	protected static $strTable = 'tl_rep';	
+}
