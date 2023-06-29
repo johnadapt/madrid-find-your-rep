7bc1248afa70de2880c6cf7f4caa1be6261d27de
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-29 15:12:07 -0400

    Create config.php

---
+++ system/modules/frasch_find_your_rep/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find Your Rep
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-your-rep
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Back end modules */
+$GLOBALS['BE_MOD']['content']['reps'] = array(
+	'tables' => array('tl_rep')
+);
+
+/* Front end modules */
+$GLOBALS['FE_MOD']['frasch_find_your_rep']['mod_find_your_rep'] 	= 'Bcs\Module\ModFindYourRep';
+
+/* Models */
+$GLOBALS['TL_MODELS']['tl_rep'] = 'Bcs\Model\Rep';
