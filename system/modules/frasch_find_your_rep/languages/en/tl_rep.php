a6a6e842cb1f6dcd8aa2f526c36d66594a8708ec
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-30 08:29:04 -0400

    Update tl_rep.php

--- system/modules/frasch_find_your_rep/languages/en/tl_rep.php
+++ system/modules/frasch_find_your_rep/languages/en/tl_rep.php
@@ -22,7 +22,7 @@ $GLOBALS['TL_LANG']['tl_rep']['address']           = array('Address', 'Enter the
 $GLOBALS['TL_LANG']['tl_rep']['city']              = array('City', 'Enter the city');
 $GLOBALS['TL_LANG']['tl_rep']['state']             = array('State', 'Enter the state');
 $GLOBALS['TL_LANG']['tl_rep']['zip']               = array('Zip', 'Enter the zip');
-$GLOBALS['TL_LANG']['tl_rep']['phone_number'] 		 = array('Phone Number', 'Enter the phone number');
+$GLOBALS['TL_LANG']['tl_rep']['phone_number'] 	   = array('Phone Number', 'Enter the phone number');
 $GLOBALS['TL_LANG']['tl_rep']['alt_phone_number']  = array('Alt. Phone Number', 'Enter the alt. phone number');
 $GLOBALS['TL_LANG']['tl_rep']['email']             = array('Email', 'Enter the email');
 $GLOBALS['TL_LANG']['tl_rep']['website']           = array('Website', 'Enter the Website');
@@ -30,8 +30,8 @@ $GLOBALS['TL_LANG']['tl_rep']['published']         = array('Published', 'Choose 
  
  /* Buttons */
 $GLOBALS['TL_LANG']['tl_rep']['new']        = array('New Rep', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_rep']['show']  		 	= array('Rep details', 'Show the details of record ID %s');
-$GLOBALS['TL_LANG']['tl_rep']['edit']  		 	= array('Edit Rep', 'Edit record ID %s');
-$GLOBALS['TL_LANG']['tl_rep']['copy']  		 	= array('Copy Rep', 'Copy record ID %s');
-$GLOBALS['TL_LANG']['tl_rep']['delete'] 		= array('Delete Rep', 'Delete record ID %s');
-$GLOBALS['TL_LANG']['tl_rep']['toggle'] 	  = array('Toggle Rep', 'Toggle record ID %s');
+$GLOBALS['TL_LANG']['tl_rep']['show']  		= array('Rep details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_rep']['edit']  		= array('Edit Rep', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_rep']['copy']  		= array('Copy Rep', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_rep']['delete']     = array('Delete Rep', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_rep']['toggle'] 	= array('Toggle Rep', 'Toggle record ID %s');
