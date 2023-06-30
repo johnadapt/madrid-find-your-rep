2a04757cd7791506830714677fe9aaf726c38840
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-30 08:31:32 -0400

    Create modules.php

---
+++ system/modules/frasch_find_your_rep/languages/en/modules.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Find Your Rep
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/frasch-find-your-rep
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['mod_find_your_rep'] 	= array('Find Your Rep', 'Module that allows filtering rep entries by State.');
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['reps'] = array('Reps', 'Configure Rep Locations');
