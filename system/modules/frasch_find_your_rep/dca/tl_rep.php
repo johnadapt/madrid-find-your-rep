2f0fc1dda2ff3cab14fb3c57d609cade2f7966ac
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-24 15:01:43 -0400

    Update tl_rep.php

--- system/modules/frasch_find_your_rep/dca/tl_rep.php
+++ system/modules/frasch_find_your_rep/dca/tl_rep.php
@@ -103,7 +103,7 @@ $GLOBALS['TL_DCA']['tl_rep'] = array
     // Palettes
     'palettes' => array
     (
-        'default'                     => '{rep_legend},rep_name,company_name,region,address,city,zip,phone_number,alt_phone_number,email,website;{state_legend},state;{publish_legend},published;'
+        'default'                     => '{rep_legend},rep_name,company_name,region,product_line,address,city,zip,phone_number,alt_phone_number,email,website;{state_legend},state;{publish_legend},published;'
     ),
  
     // Fields
@@ -142,6 +142,15 @@ $GLOBALS['TL_DCA']['tl_rep'] = array
         'region' => array
     		(
     			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['region'],
+    			'inputType'               => 'text',
+    			'default'                 => '',
+    			'search'                  => true,
+    			'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    			'sql'                     => "varchar(255) NOT NULL default ''"
+    		),
+        'product_line' => array
+    		(
+    			'label'                   => &$GLOBALS['TL_LANG']['tl_rep']['product_line'],
     			'inputType'               => 'text',
     			'default'                 => '',
     			'search'                  => true,
