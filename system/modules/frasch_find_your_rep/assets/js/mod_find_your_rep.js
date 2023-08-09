e670820e7c50bbc5b96794710e5ff45c554c2a84
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-09 09:15:27 -0400

    Update mod_find_your_rep.js

--- system/modules/frasch_find_your_rep/assets/js/mod_find_your_rep.js
+++ system/modules/frasch_find_your_rep/assets/js/mod_find_your_rep.js
@@ -1,3 +1,81 @@
-$(document).ready(function() {
-  //alert("Bing Bong Noise");
+
+  
+// When the page is loaded
+$( document ).ready(function() {
+    
+    // When our 'select_your_state' select changes
+    $( ".select_your_state" ).on( "change", function() {
+
+        // store our selected state
+        var selectedState = this.value;
+        
+        // show our product line filter once a state is selected
+        if(selectedState != '') {
+            $('.select_product_line').fadeIn();
+        } else {
+            $('.select_product_line').fadeOut();
+        }
+        
+        // Loop through each listing
+        $( ".rep_list .rep" ).each(function() {
+            
+            // If this rep has our selected state in it's class list, show the rep
+            if($(this).hasClass(selectedState)) {
+                
+                // Get our product line filter value
+                var selectedProductLine = $( ".product_line option:selected" ).val();
+                
+                if(selectedProductLine == '')
+                    // if there is nothing selected then show our rep
+                    $(this).fadeIn();
+                else {
+                    // if there is something selected see if it matches our listing
+                    if($(this).data('product-line') == selectedProductLine)
+                        $(this).fadeIn();
+                    else
+                        $(this).fadeOut();
+                }
+            } else {
+                $(this).fadeOut();
+            }
+        });
+        
+    });
+    
+    
+    // When our 'select_your_state' select changes
+    $( ".product_line" ).on( "change", function() {
+        
+        // store our selected product line
+        var selectedProductLine = this.value;
+        
+        // Loop through each listing
+        $( ".rep_list .rep" ).each(function() {
+
+            // If this rep has our selected state in it's class list, show the rep
+            if($(this).data('product-line') == selectedProductLine) {
+                
+                // Get our selected state
+                var selectedState = $( ".select_your_state option:selected" ).val();
+                
+                if(selectedState == '') {
+                    // if our selected state is empty show our listing
+                    $(this).fadeIn();
+                } else {
+                    // if we selected a state, make sure the state is in the class list
+                    if($(this).hasClass(selectedState))
+                        $(this).fadeIn();
+                    else
+                        $(this).fadeOut();
+                }
+
+            } else {
+                $(this).fadeOut();
+            }
+        });
+
+    });
+    
+    
+    
 });
