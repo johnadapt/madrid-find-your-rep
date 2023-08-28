785282f5428d3cf21a7990572a33745ef774484b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-28 14:38:58 -0400

    Update mod_find_your_rep.js

--- system/modules/frasch_find_your_rep/assets/js/mod_find_your_rep.js
+++ system/modules/frasch_find_your_rep/assets/js/mod_find_your_rep.js
@@ -38,7 +38,7 @@ $( document ).ready(function() {
                     
                 else {
                     // if there is something selected see if it matches our listing
-                    if($(this).data('product-line') == selectedProductLine) {
+                    if($(this).data('product-line').includes(selectedProductLine)) {
                         $(this).show();
                         entry_found = true;
                     }
@@ -69,7 +69,7 @@ $( document ).ready(function() {
         $( ".rep_list .rep" ).each(function() {
 
             // If this rep has our selected state in it's class list, show the rep
-            if($(this).data('product-line') == selectedProductLine) {
+            if($(this).data('product-line').includes(selectedProductLine)) {
                 
                 // Get our selected state
                 var selectedState = $( ".select_your_state option:selected" ).val();
