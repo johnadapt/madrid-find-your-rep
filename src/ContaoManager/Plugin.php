f35964dc6749787140031e0bacb7e09ded9050d8
johnadapt <2372574+johnadapt@users.noreply.github.com>
2024-06-14 11:10:34 -0700

    initial conversion to package

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-recaptcha
+ */
+
+namespace Bcs\MadridFindRepBundle\ContaoManager;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Bcs\MadridFindRepBundle\BcsMadridFindRepBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}
