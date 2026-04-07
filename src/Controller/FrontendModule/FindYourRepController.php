7997e8647d995504e9a14d526097b86fab4c21d5
johnadapt <2372574+johnadapt@users.noreply.github.com>
2026-04-06 21:11:41 -0700

    again

--- src/Controller/FrontendModule/FindYourRepController.php
+++ src/Controller/FrontendModule/FindYourRepController.php
@@ -71,6 +71,31 @@ class FindYourRepController extends Module
             return;
         }
 
+        // Maps state key (e.g. 'st1') to array of HTML comment strings.
+        // Populated while rendering rep items and passed to the module template
+        // as JSON, so map.setComment() can be called after map is initialised.
+        $arrMapComments = [];
+
+        $stateKeyMap = [
+            'alabama' => 'st1', 'alaska' => 'st2', 'arizona' => 'st3',
+            'arkansas' => 'st4', 'california' => 'st5', 'colorado' => 'st6',
+            'connecticut' => 'st7', 'delaware' => 'st8', 'washington_dc' => 'st9',
+            'florida' => 'st10', 'georgia' => 'st11', 'hawaii' => 'st12',
+            'idaho' => 'st13', 'illinois' => 'st14', 'indiana' => 'st15',
+            'iowa' => 'st16', 'kansas' => 'st17', 'kentucky' => 'st18',
+            'louisiana' => 'st19', 'maine' => 'st20', 'maryland' => 'st21',
+            'massachusetts' => 'st22', 'michigan' => 'st23', 'minnesota' => 'st24',
+            'mississippi' => 'st25', 'missouri' => 'st26', 'montana' => 'st27',
+            'nebraska' => 'st28', 'nevada' => 'st29', 'new_hampshire' => 'st30',
+            'new_jersey' => 'st31', 'new_mexico' => 'st32', 'new_york' => 'st33',
+            'north_carolina' => 'st34', 'north_dakota' => 'st35', 'ohio' => 'st36',
+            'oklahoma' => 'st37', 'oregon' => 'st38', 'pennsylvania' => 'st39',
+            'rhode_island' => 'st40', 'south_carolina' => 'st41', 'south_dakota' => 'st42',
+            'tennessee' => 'st43', 'texas' => 'st44', 'utah' => 'st45',
+            'vermont' => 'st46', 'virginia' => 'st47', 'washington' => 'st48',
+            'west_virginia' => 'st49', 'wisconsin' => 'st50', 'wyoming' => 'st51',
+        ];
+
         $arrReps = [];
 
         while ($objReps->next()) {
@@ -96,9 +121,30 @@ class FindYourRepController extends Module
             $objTemplate = new FrontendTemplate($strItemTemplate);
             $objTemplate->setData($arrLocation);
             $arrReps[] = $objTemplate->parse();
+
+            // Build the tooltip HTML for each assigned state so we can call
+            // map.setComment() AFTER map is initialised (via mapComments JSON).
+            $repHtml  = '<div class="rep_wrap">';
+            if ($objReps->region)           { $repHtml .= '<p class="territory">'   . htmlspecialchars($objReps->region)                                                  . '</p>'; }
+            if ($objReps->rep_name)         { $repHtml .= '<p class="rep_name">'    . htmlspecialchars($objReps->rep_name)                                                . '</p>'; }
+            if ($objReps->company_name)     { $repHtml .= '<p class="company_name">'. htmlspecialchars($objReps->company_name)                                            . '</p>'; }
+            if ($objReps->address)          { $repHtml .= '<p class="address">'     . htmlspecialchars($objReps->address) . '<br>' . htmlspecialchars($objReps->city) . ', ' . htmlspecialchars($objReps->address_state) . '&nbsp;&nbsp;' . htmlspecialchars($objReps->zip) . '</p>'; }
+            if ($objReps->phone_number)     { $repHtml .= '<p class="phone_number">Phone: <a href="tel:' . htmlspecialchars($objReps->phone_number) . '">' . htmlspecialchars($objReps->phone_number) . '</a></p>'; }
+            if ($objReps->alt_phone_number) { $repHtml .= '<p class="alt_phone_number">Alt Phone: <a href="tel:' . htmlspecialchars($objReps->alt_phone_number) . '">' . htmlspecialchars($objReps->alt_phone_number) . '</a></p>'; }
+            if ($objReps->email)            { $repHtml .= '<p class="email">Email: <a href="mailto:' . htmlspecialchars($objReps->email) . '">' . htmlspecialchars($objReps->email) . '</a></p>'; }
+            if ($objReps->website)          { $repHtml .= '<p class="email">Website: <a href="' . htmlspecialchars($objReps->website) . '" target="_blank" rel="nofollow">' . htmlspecialchars($objReps->website) . '</a></p>'; }
+            $repHtml .= '</div>';
+
+            foreach ($arrLocation['state'] as $stateName) {
+                $stateKey = $stateKeyMap[$stateName] ?? null;
+                if ($stateKey) {
+                    $arrMapComments[$stateKey][] = $repHtml;
+                }
+            }
         }
 
-        $this->Template->reps  = $arrReps;
-        $this->Template->empty = '';
+        $this->Template->reps        = $arrReps;
+        $this->Template->empty       = '';
+        $this->Template->mapComments = json_encode($arrMapComments);
     }
 }
